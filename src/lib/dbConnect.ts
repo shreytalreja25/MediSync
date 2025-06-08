e30b08f317cf292ceb308ff62a9067294a6a5a56
shreytalreja25 <shreytalreja25@gmail.com>
2025-06-08 21:01:56 +1000

    Fixing MongoDB Issues

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -1,6 +1,9 @@
 import mongoose from 'mongoose'
 
-const MONGODB_URI = process.env.MONGODB_URI as string
+// Hardcoded MongoDB URI for debugging
+const MONGODB_URI = 'mongodb+srv://shreytalreja25:Shrey%409999@cluster0.7lbnxum.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+
+console.log('Using MongoDB URI:', MONGODB_URI)
 
 if (!MONGODB_URI) {
   throw new Error('Please define the MONGODB_URI environment variable in .env.local')
